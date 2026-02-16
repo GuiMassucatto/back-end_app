8398e961ab63831b65910b1048630ab0a94654b6
GuiMassucatto <guimassucatto@outlook.com>
2026-02-16 16:52:08 -0300

    quarto commit

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./src/routes')
+const routes = require('./src/routes');
 
 const app = express();
-app.use(express.json())
-app.use(cors())
-app.use(routes)
 
-app.listen(3000, (req,res) =>{
-    console.log('Server rodando na porta 3000') 
-})
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+module.exports = app;
