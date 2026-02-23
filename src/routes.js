4c54800dce024996b2d937fd020d7a9cb5227686
Guilherme Massucatto <guimassucatto@outlook.com>
2026-02-23 19:09:28 -0300

    ajustando os horarios

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,5 @@ const agendamento = require('./controllers/agendamentoController');
 routes.get('/agendamentos', agendamento.read);
 routes.put('/agendamentos/:id/reservar', agendamento.reservar);
 routes.put('/agendamentos/:id/liberar', agendamento.liberar);
-router.delete('/agendamentos/:id', controller.deletarUm);
 
 module.exports = routes
